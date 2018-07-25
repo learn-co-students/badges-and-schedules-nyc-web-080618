bb4b25092fa64784b60b95365515b9414e221978
JUN JIAN SHEN <jshen9085@gmail.com>
2018-07-25 02:09:32 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,32 @@
 # Write your code here.
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(array)
+  new_array = []
+  array.each do |name|
+    new_array.push("Hello, my name is #{name}.")
+  end
+  return new_array
+end
+
+def assign_rooms(array)
+  array2 = []
+  counter = 1
+  array.each do |name|
+    array2.push("Hello, #{name}! You'll be assigned to room #{counter}!")
+    counter += 1
+  end
+  return array2
+end
+
+def printer(array)
+  batch_badge_creator(array).each do |id|
+    puts id
+  end 
+  
+  assign_rooms(array).each do |id|
+    puts id 
+  end
+end 
