f5d26c79e278cd5647d0e1d81259542b602db24b
Kristina Tong <kristina.tong@gmail.com>
2018-08-02 20:57:04 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,28 @@
-# Write your code here.
+def badge_maker(string)
+  "Hello, my name is #{string}."
+end
+
+def batch_badge_creator(array)
+  badge_array = []
+  for i in (0...array.length)
+    badge_array.push("Hello, my name is #{array[i]}.")
+  end
+  badge_array
+end
+
+def assign_rooms(array)
+  badge_array = []
+  for i in (0...array.length)
+    badge_array.push("Hello, #{array[i]}! You'll be assigned to room #{i+1}!")
+  end
+  badge_array
+end
+
+def printer(array)
+  array1 = batch_badge_creator(array)
+  array2 = assign_rooms(array)
+  for i in (0...array.length)
+    puts array1[i]
+    puts array2[i]
+  end
+end
